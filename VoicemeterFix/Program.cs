312d206c598526652410877f5177629faab13e41
Umbra <69671761+Umbra999@users.noreply.github.com>
2023-02-06 08:12:38 +0100

    Release

---
+++ VoicemeterFix/Program.cs
@@ -0,0 +1,14 @@
+﻿using System.Diagnostics;
+
+Console.WriteLine($"Waiting for Audio Tasks");
+Process[] audiodg = null;
+do
+{
+    audiodg = Process.GetProcessesByName("audiodg");
+} while (audiodg == null || audiodg.Length == 0);
+
+Console.WriteLine($"Found {audiodg.Length} Windows Audio Processes");
+foreach (Process process in audiodg)
+{
+    process.ProcessorAffinity = (IntPtr)1;
+}
