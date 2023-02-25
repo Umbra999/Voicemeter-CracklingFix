78d9e20fe820355b2710feb7417efaf4697ed9d4
Umbra <69671761+Umbra999@users.noreply.github.com>
2023-02-25 15:48:47 +0100

    s

--- VoicemeterFix/Program.cs
+++ VoicemeterFix/Program.cs
@@ -1,14 +1,33 @@
 ﻿using System.Diagnostics;
+using System.Security.Principal;
 
-Console.WriteLine($"Waiting for Audio Tasks");
-Process[] audiodg = null;
-do
+WindowsIdentity identity = WindowsIdentity.GetCurrent();
+WindowsPrincipal principal = new WindowsPrincipal(identity);
+if (!principal.IsInRole(WindowsBuiltInRole.Administrator))
 {
-    audiodg = Process.GetProcessesByName("audiodg");
-} while (audiodg == null || audiodg.Length == 0);
 
-Console.WriteLine($"Found {audiodg.Length} Windows Audio Processes");
-foreach (Process process in audiodg)
+    ProcessStartInfo startInfo = new ProcessStartInfo
+    {
+        FileName = Environment.GetCommandLineArgs()[0],
+        UseShellExecute = true,
+        Verb = "runas",
+        Arguments = "/runas"
+    };
+
+    Process.Start(startInfo);
+}
+else
 {
-    process.ProcessorAffinity = (IntPtr)1;
+    Console.WriteLine($"Waiting for Audio Tasks");
+    Process[] audiodg = null;
+    do
+    {
+        audiodg = Process.GetProcessesByName("audiodg");
+    } while (audiodg == null || audiodg.Length == 0);
+
+    Console.WriteLine($"Found {audiodg.Length} Windows Audio Processes");
+    foreach (Process process in audiodg)
+    {
+        process.ProcessorAffinity = (IntPtr)1;
+    }
 }
